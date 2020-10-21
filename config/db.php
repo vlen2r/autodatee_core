a8b3f42757981c05b7b0fb82d48d80da43b1d28d
SISE <sise@ub18.fibertel.com.ar>
2020-10-21 18:52:16 +0000

    se modificaron los permisos de todos los archivos y carpetas para que sea ejecutable en un ambiente web.

--- config/db.php
+++ config/db.php
@@ -2,10 +2,17 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql;dbname=databas',
-    'username' => 'root',
-    'password' => 'Sise.2016',
+//    'dsn' => 'mysql:host=mysql;dbname=databas',
+//    'username' => 'root',
+//    'password' => 'Sise.2016',
+//    'charset' => 'utf8',
+
+//ambiente desarrollo
+    'dsn' => 'mysql:host=mysql;dbname=autodatee',
+    'username' => 'docker',
+    'password' => 'docker',
     'charset' => 'utf8',
+
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
