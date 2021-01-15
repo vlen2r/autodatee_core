9ef6fc7f486e7a3d40ff6292a0e680450161e782
Sebas <sebatista@yahoo.com>
2021-01-15 08:38:14 -0300

    Actualización de Datos
    Se agrega la información de conexión al ambiente de producción.
    
    Se modifica la ubicación de la plantilla csv al root del proyecto.

--- config/db.php
+++ config/db.php
@@ -2,17 +2,25 @@
 
 return [
     'class' => 'yii\db\Connection',
+
+
+//ambiente de test CPanel autodateetest.siseservicios.com/web
+    'dsn' => 'mysql:host=localhost;dbname=siseserv_autodateetest',
+    'username' => 'siseserv_autodatee',
+    'password' => 'sise.2016',
+    'charset' => 'utf8',
+
+//ambiente de desarrollo 2021
+//    'dsn' => 'mysql:host=mysql;dbname=siseserv_autodatee',
+//    'username' => 'docker',
+//    'password' => 'docker',
+//    'charset' => 'utf8',
+	
+//ambiente de desarrollo 2020
 //    'dsn' => 'mysql:host=mysql;dbname=databas',
 //    'username' => 'root',
 //    'password' => 'Sise.2016',
 //    'charset' => 'utf8',
-
-//ambiente desarrollo
-    'dsn' => 'mysql:host=mysql;dbname=siseserv_autodatee',
-    'username' => 'docker',
-    'password' => 'docker',
-    'charset' => 'utf8',
-
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
