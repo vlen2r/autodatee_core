5e4e5cb24f81244ce177d6d41a6bbbc90fffe431
Sebas <sebatista@yahoo.com>
2021-07-27 23:20:43 -0300

    Exportación a Excel. 3ra Prueba
    Se agrega una tercera forma de exportar.
    según link:
    https://github.com/kartik-v/yii2-export

--- views/historial/index.php
+++ views/historial/index.php
@@ -9,6 +9,7 @@ use yii\helpers\Html;
  */
 //use arturoliveira\ExcelView;
 use kartik\grid\GridView;
+use kartik\export\ExportMenu;
 //2021-07-27 - End of the add by Batista
 
 /* @var $this yii\web\View */
@@ -25,45 +26,33 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); 
     ?>
 
-    <?= GridView::widget([
+<?= 
+
+    $gridColumns = [
+        ['class' => 'yii\grid\SerialColumn'],
+        'cliente.nombre',
+        'cantidad',
+        'fecha',
+        /**
+         * Agregado por Batista 2021-07-26
+         * Para filtrar mediante el nombre del cliente.
+         * Siendo cliente una tabla foranea de Historial.
+         */
+        'clienteNombre',
+        //Fin del agregado Batista 2021-07-26
+        /*['class' => 'yii\grid\ActionColumn'],*/
+    ];
+
+    ExportMenu::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $gridColumns,
+        'clearBuffers' => true, //optional
+    ]);
+
+    GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'cliente.nombre',
-            'cantidad',
-            'fecha',
-            /**
-             * Agregado por Batista 2021-07-26
-             * Para filtrar mediante el nombre del cliente.
-             * Siendo cliente una tabla foranea de Historial.
-             */
-            'clienteNombre',
-            //Fin del agregado Batista 2021-07-26
-            /*['class' => 'yii\grid\ActionColumn'],*/
-        ],
-        //'layout' => '{summary}<div class="pull-right">{export}&nbsp{fullexport}&nbsp</div><div>{items}</div>{pager}',
-        'layout' => '{summary}<div class="pull-right">{export}&nbsp</div><div>{items}</div>{pager}',
-        /*
-        'exportConfig' => [
-            \kartik\grid\GridView::EXCEL => ['label' => 'Export to Excel'],
-        ],
-        */
-        /*
-        'fullExportConfig' => [
-            ExcelView::FULL_EXCEL => [],
-            //ExcelView::FULL_CSV => ['label' => 'Save as CSV'],
-            //ExcelView::FULL_HTML => [],
-        ],
-        */
-        'toolbar' =>  [
-            ['content'=>
-                Html::button('&lt;i class="glyphicon glyphicon-plus">&lt;/i>', ['type'=>'button', 'title'=>'Add Book', 'class'=>'btn btn-success', 'onclick'=>'alert("This will launch the book creation form.\n\nDisabled for this demo!");']) . ' '.
-                Html::a('&lt;i class="glyphicon glyphicon-repeat">&lt;/i>', ['grid-demo'], ['data-pjax'=>0, 'class' => 'btn btn-default', 'title'=>'Reset Grid'])
-            ],
-            '{export}',
-            '{toggleData}'
-        ],
+        'columns' => $gridColumns,
     ]); ?>
 
 
