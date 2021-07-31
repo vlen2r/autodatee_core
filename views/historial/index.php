4ad187bbe03b3319bb300e1619f3e03de218dc6e
Sebas <sebatista@yahoo.com>
2021-07-31 14:27:25 -0300

    Update index.php
    Se actualiza el index del historial para porder exportar a Excel.
    Link github:
    https://github.com/kartik-v/yii2-export
    Link ejemplo:
    https://demos.krajee.com/export-demo-dtl/specific-formats
    
    Se modificó la forma de presentar cada widget, inexplicablemente hay que poner todo dentro de un mismo <?php ?>

--- views/historial/index.php
+++ views/historial/index.php
@@ -27,36 +27,56 @@ $this->params['breadcrumbs'][] = $this->title;
     ?>
 
 <?= 
-/* Me tira el error 
+// Me tira el error 
+/*
     $gridColumns = [
-        ['class' => 'yii\grid\SerialColumn'],       Array to string conversion
-        'id',
-        'name',
-        'color',
-        'publish_date',
-        'status',
-        ['class' => 'yii\grid\ActionColumn'],
+        ['class' => 'yii\grid\SerialColumn'],      // Array to string conversion
+        'cliente.nombre',
+        'cantidad',
+        'fecha',
+        'clienteNombre',
+        //['class' => 'yii\grid\ActionColumn'],
     ];
 */
     ExportMenu::widget([
         'dataProvider' => $dataProvider,
+        //'columns' => $gridColumns,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
             'cliente.nombre',
             'cantidad',
             'fecha',
-            /**
-             * Agregado por Batista 2021-07-26
-             * Para filtrar mediante el nombre del cliente.
-             * Siendo cliente una tabla foranea de Historial.
-             */
+            //Agregado por Batista 2021-07-26. Para filtrar mediante el nombre del cliente. Siendo cliente una tabla foranea de Historial.
             'clienteNombre',
             //Fin del agregado Batista 2021-07-26
-            /*['class' => 'yii\grid\ActionColumn'],*/
+            //['class' => 'yii\grid\ActionColumn'],
         ],
         'clearBuffers' => true, //optional
-    ]);
 
+        'exportConfig' => [
+            ExportMenu::FORMAT_TEXT => false,
+            ExportMenu::FORMAT_HTML => false,
+            ExportMenu::FORMAT_EXCEL => false,
+            ExportMenu::FORMAT_PDF => [
+                'pdfConfig' => [
+                    'methods' => [
+                        'SetTitle' => 'Grid Export - Krajee.com',
+                        'SetSubject' => 'Generating PDF files via yii2-export extension has never been easy',
+                        'SetHeader' => ['Krajee Library Export||Generated On: ' . date("r")],
+                        'SetFooter' => ['|Page {PAGENO}|'],
+                        'SetAuthor' => 'Kartik Visweswaran',
+                        'SetCreator' => 'Kartik Visweswaran',
+                        'SetKeywords' => 'Krajee, Yii2, Export, PDF, MPDF, Output, GridView, Grid, yii2-grid, yii2-mpdf, yii2-export',
+                    ]
+                ]
+            ],
+        ],
+        'dropdownOptions' => [
+            'label' => 'Export All',
+            'class' => 'btn btn-outline-secondary'
+        ],
+    ]) . "<hr>\n".
+    // Se comenta porque el export ya tiene su propia grid.
     GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -65,16 +85,15 @@ $this->params['breadcrumbs'][] = $this->title;
             'cliente.nombre',
             'cantidad',
             'fecha',
-            /**
-             * Agregado por Batista 2021-07-26
-             * Para filtrar mediante el nombre del cliente.
-             * Siendo cliente una tabla foranea de Historial.
-             */
+            //Agregado por Batista 2021-07-26. Para filtrar mediante el nombre del cliente.
+            //Siendo cliente una tabla foranea de Historial.
             'clienteNombre',
             //Fin del agregado Batista 2021-07-26
-            /*['class' => 'yii\grid\ActionColumn'],*/
+            //['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
+    ]); 
+    
+?>
 
 
 </div>
